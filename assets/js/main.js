9945cdc2bc19cd18053b2b069ea6d649b3756d71
Falco Nogatz <fnogatz@gmail.com>
2021-09-05 17:24:19 +0200

    Load GitHub stats via AJAX API calls

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,21 @@ $(document).ready(function () {
       bFilter: false,
     },
   });
+
+  // Display GitHub stats
+  document.querySelectorAll("#projects .repo").forEach((repo) => {
+    const repoUrl = repo.querySelector("a").getAttribute("href");
+    const repoApiUrl = repoUrl.replace("github.com", "api.github.com/repos");
+    fetch(repoApiUrl)
+      .then((res) => res.json())
+      .then(({ stargazers_count, forks_count }) => {
+        if (stargazers_count) {
+          repo.querySelector(".stars").textContent = stargazers_count;
+        }
+
+        if (forks_count) {
+          repo.querySelector(".forks").textContent = forks_count;
+        }
+      });
+  });
 });
