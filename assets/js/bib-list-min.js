0b9938a01ec7935ab8071715deca7d4b57ec9c60
Falco Nogatz <fnogatz@gmail.com>
2022-06-17 12:32:05 +0200

    Add new publication: School's Out

--- assets/js/bib-list-min.js
+++ assets/js/bib-list-min.js
@@ -4441,19 +4441,13 @@ var bibtexify = (function ($) {
       return itemStr;
     },
     inproceedings: function (entryData) {
-      return (
-        this.authors2html(entryData.author) +
-        " (" +
-        entryData.year +
-        "). " +
-        entryData.title +
-        ". In <em>" +
-        entryData.booktitle +
-        ", pp. " +
-        entryData.pages +
-        (entryData.address ? ", " + entryData.address : "") +
-        ".</em>"
-      );
+      return `${this.authors2html(entryData.author)} (${entryData.year}) ${
+        entryData.title
+      }. In <em>${entryData.booktitle}${
+        entryData.pages ? `, pp. ${entryData.pages}` : ""
+      }${entryData.address ? `, ` + entryData.address : ""}</em>${
+        entryData.note ? ` ${entryData.note}.` : ""
+      }`;
     },
     incollection: function (entryData) {
       return (
